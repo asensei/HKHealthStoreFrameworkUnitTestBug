68b681fdd0216646c1da03e824f2e2671f50ef7b
Bill Burgar <bill.burgar@asensei.com>
2016-09-22 13:59:04 +0100

    Initial Commit

---
+++ HKHealthStoreFrameworkUnitTestBug/HKHealthStoreFrameworkUnitTestBug.h
@@ -0,0 +1,19 @@
+//
+//  HKHealthStoreFrameworkUnitTestBug.h
+//  HKHealthStoreFrameworkUnitTestBug
+//
+//  Created by William Burgar on 22/09/2016.
+//  Copyright © 2016 asensei inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKHealthStoreFrameworkUnitTestBug.
+FOUNDATION_EXPORT double HKHealthStoreFrameworkUnitTestBugVersionNumber;
+
+//! Project version string for HKHealthStoreFrameworkUnitTestBug.
+FOUNDATION_EXPORT const unsigned char HKHealthStoreFrameworkUnitTestBugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKHealthStoreFrameworkUnitTestBug/PublicHeader.h>
+
+
---
+++ HKHealthStoreFrameworkUnitTestBugTests/HKHealthStoreFrameworkUnitTestBugTests.m
@@ -0,0 +1,39 @@
+//
+//  HKHealthStoreFrameworkUnitTestBugTests.m
+//  HKHealthStoreFrameworkUnitTestBugTests
+//
+//  Created by William Burgar on 22/09/2016.
+//  Copyright © 2016 asensei inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HKHealthStoreFrameworkUnitTestBugTests : XCTestCase
+
+@end
+
+@implementation HKHealthStoreFrameworkUnitTestBugTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
